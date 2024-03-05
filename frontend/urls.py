b051d0a056ded75624a49f7c088516b72c25ed8a
hsualexotake <147958762+hsualexotake@users.noreply.github.com>
2024-03-05 16:04:51 +0100

    Add files via upload

---
+++ frontend/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from .views import index
+
+
+urlpatterns = [
+     
+    path('', index),
+    path('join', index),
+    path('create', index),
+    path('room/<str:roomCode>', index),
+   # path('room', index),
+    
+
+]
